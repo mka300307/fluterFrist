8ade3ddfc68cac79db86f20baaf49bc7730481db
mka300307 <muhammadkhaisar819@gmail.com>
2023-08-21 09:21:17 +0700

    feat [mka]listView HomePage

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,34 +1,136 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<HomePage> createState() => _HomeState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomeState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text ("Data Siswa"),),
-      body: ListView(
-        children: [
-          Row(
+      body: Center(
+          child: ListView(
+            padding: const EdgeInsets.all(8),
             children: [
-              Column(
-                children: [Text("Nama Siswa"),
-                Text("Altan Naura")],
-              )
+              adapterList()
             ],
           )
-        ],
+      ),
+    );
+  }
 
-
+  adapterList(){
+    return <Widget>[
+      Container(
+        height: 50,
+        color: Colors.amber[600],
+        child: const Center(child: Text('Entry A')),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[500],
+        child: const Center(child: Text('Entry B')),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[100],
+        child: const Center(child: Text('Entry C')),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[100],
+        child: const Center(child: Text('Entry C')),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[100],
+        child: const Center(child: Text('Entry C')),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[100],
+        child: const Center(child: Text('Entry C')),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[100],
+        child: const Center(child: Text('Entry C')),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[100],
+        child: const Center(child: Text('Entry C')),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[100],
+        child: const Center(child: Text('Entry C')),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[100],
+        child: const Center(child: Text('Entry C')),
       ),
 
-    );
 
+      Container(
+        height: 50,
+        color: Colors.amber[100],
+        child: const Center(child: Text('Entry C')),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[100],
+        child: const Center(child: Text('Entry C')),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[100],
+        child: const Center(child: Text('Entry C')),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[100],
+        child: const Center(child: Text('Entry C')),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[100],
+        child: const Center(child: Text('Entry C')),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[100],
+        child: const Center(child: Text('Entry C')),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[100],
+        child: const Center(child: Text('Entry C')),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[100],
+        child: const Center(child: Text('Entry C')),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[100],
+        child: const Center(child: Text('Entry C')),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[100],
+        child: const Center(child: Text('Entry C')),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[100],
+        child: const Center(child: Text('Entry C')),
+      ),
+    ];
   }
 }
